029a9f0d76cdb9fd6555d6b85a090ce1188fdbc9
Jonathan Cruickshank <ikenna1982@hotmail.com>
2018-04-10 19:20:16 +0100

    adds add marker and add click event

--- public/app.js
+++ public/app.js
@@ -1,4 +1,9 @@
 const initialize = function() {
-  let map = new MapWrapper();
-    
+  const center = {lat: 53, lng: -2};
+  const container = document.querySelector("#main-map");
+  let map = new MapWrapper(container, center, 10);
+  map.addMarker(center);
+  map.addClickEvent();
 }
+
+window.addEventListener("load", initialize);
--- public/mapWrapper.js
+++ public/mapWrapper.js
@@ -1,7 +1,23 @@
-const MapWrapper = function(coords, zoom) {
-  const container = document.querySelector("#main-map");
+const MapWrapper = function(container, coords, zoom) {
   this.googleMap = new google.maps.Map(container, {
     center: coords,
-    zoom: zoom);
-  }
+    zoom: zoom
+  });
 }
+
+MapWrapper.prototype.addMarker = function (coords) {
+  const marker = new google.maps.Marker({
+    position: coords,
+    map: this.googleMap
+  });
+};
+
+MapWrapper.prototype.addClickEvent = function () {
+  google.maps.event.addListener(this.googleMap, "click", function(event) {
+    const lat = event.latLng.lat();
+    const lng = event.latLng.lng();
+    coords = {lat: lat, lng: lng }
+    this.addMarker(coords);
+  }.bind(this));
+
+};
